df2ce4dd246188835c549b64a17ae44adeaf3a60
Harald Sack <harald.sack@t-systems.com>
2025-05-08 14:50:36 +0200

    spring boot example with logging

--- samples-doc/doc-sample-springboot-rest/src/main/java/com/opentelekomcloud/samples/springboot/GreetingController.java
+++ samples-doc/doc-sample-springboot-rest/src/main/java/com/opentelekomcloud/samples/springboot/GreetingController.java
@@ -2,18 +2,36 @@ package com.opentelekomcloud.samples.springboot;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.opentelekomcloud.samples.springboot.config.OTCRequestContextFilter;
+
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+  private static Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
+  private static final String templateGreeting = "Hello, %s!";
+  private static final String templateByeBye = "Bye bye, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  @GetMapping("/greeting")
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+    logger.info("RequestID: {} Name: {}",  OTCRequestContextFilter.requestId.get(), name);
+
+    return new Greeting(counter.incrementAndGet(), String.format(templateGreeting, name));
+  }
+
+  @GetMapping("/byebye")
+  public Greeting byebye(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+    logger.info("RequestID: {} Name: {}",  OTCRequestContextFilter.requestId.get(), name);
+
+    return new Greeting(counter.incrementAndGet(), String.format(templateByeBye, name));
+  }
 }
